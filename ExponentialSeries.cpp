3f914beeda59d4f8ca41e2041e360c081955015f
KarlAnna <annkarlusha@gmail.com>
2024-12-29 08:52:56 +0000

    Hotfix: FuncA calculates the first 3 terms of exponential series

--- ExponentialSeries.cpp
+++ ExponentialSeries.cpp
@@ -8,9 +8,9 @@ double ExponentialSeries::FuncA() {
     double term = 1.0;
     double x = 1.0;
     
-    for (int i = 1; i <= 1; ++i) {
+    for (int i = 1; i <= 3; ++i) {
         term = term * x / i;
         sum += term;
     }
-    return 0;
+    return sum;
 }
